fc19eeee51b51794478ca65b13500413e3ffd7ad
María Barragán Pantoja <maria.barragan80@soy.sena.edu.co>
2024-03-11 13:03:09 -0500

    se hacen routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListComentariosComponent } from './components/list-comentarios/list-comentarios.component';
+import { AgregarEditarComentarioComponent } from './components/agregar-editar-comentario/agregar-editar-comentario.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ListComentariosComponent },
+  { path: 'agregar', component: AgregarEditarComentarioComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
